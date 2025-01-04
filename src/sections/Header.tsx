88fd2a847d82051107e920b863323a6399f30f8f
xdmy1 <152696259+xdmy1@users.noreply.github.com>
2025-01-04 18:50:33 +0200

    Update Header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -8,10 +8,10 @@ export const Header = () => {
     <header className="sticky top-0 backdrop-blur-sm z-20">
       <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
         <p className="text-white/60 hidden md:block">
-          Streamline your workflow and boost your productivity
+          Daca nu ai bani suna-ma.(nici eu n am)
         </p>
         <div className="inline-flex gap-1 items-center">
-          <p>Get started for free</p>
+          <p>Mai stam de vorba</p>
           <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
         </div>
       </div>
